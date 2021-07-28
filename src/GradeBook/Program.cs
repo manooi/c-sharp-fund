ee69c9cd69cfdea8ebe6facee59426f840781e11
manooi <sirawit.mn@gmail.com>
2021-07-28 12:25:13 +0700

    Module 3: Learning the C# Syntax

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // var numbers = new double[] { 1.0, 2.123, 123.44, 12.3 };
+      // var numbers = new [] { 1.0, 2.123, 123.44, 12.3 };
+
+      // List<double> grades = new List<double>();
+      var grades = new List<double>();
+      grades.Add(5.0);
+      grades.Add(1.0);
+      grades.Add(2.0);
+
+      double result = 0.0;
+
+      foreach (var item in grades)
+      {
+        result += item;
+      }
+
+      Console.WriteLine($"The average grade is {(result / grades.Count):N1}");
+
+      // if (args.Length > 0)
+      // {
+      //   Console.WriteLine($"Hello, {args[0]}!");
+      // }
+      // else
+      // {
+      //   Console.WriteLine("Hello!");
+      // }
+    }
+  }
+}
