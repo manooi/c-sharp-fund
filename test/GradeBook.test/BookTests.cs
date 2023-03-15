0a9a456ad571442df34f671e372823a44c6dd5b1
sirawit.mahanin <sirawit.mahanin@accenture.com>
2023-03-15 22:14:46 +0700

    Rework on unit tests

--- test/GradeBook.test/BookTests.cs
+++ test/GradeBook.test/BookTests.cs
@@ -7,38 +7,38 @@ namespace GradeBook.Test
 
 
 
- public class BookTests
- {
+    public class BookTests
+    {
 
 
 
-  [Fact]
-  public void BookCalculatesAnAverageGrades()
-  {
+        [Fact]
+        public void BookCalculatesAnAverageGrades()
+        {
 
-   // arrange
-   var book = new InMemmoryBook("Sirawit");
+            // arrange
+            var book = new InMemmoryBook("Sirawit");
 
-   book.AddGrade(90.0);
-   book.AddGrade(85.0);
-   book.AddGrade(70.0);
+            book.AddGrade(90.0);
+            book.AddGrade(85.0);
+            book.AddGrade(70.0);
 
-   // act
-   var result = book.GetStatistics();
+            // act
+            var result = book.GetStatistics();
 
-   // assert
-   Assert.Equal(90.0, result.High);
-   Assert.Equal(81.66, result.Average, 1);
-   Assert.Equal(70.0, result.Low);
-   Assert.Equal('B', result.Letter);
-  }
+            // assert
+            Assert.Equal(90.0, result.High);
+            Assert.Equal(81.66, result.Average, 1);
+            Assert.Equal(70.0, result.Low);
+            Assert.Equal('B', result.Letter);
+        }
 
-  [Fact]
-  public void TestAddGrade()
-  {
-   var book = new InMemmoryBook("Grade Book");
-   book.AddGrade(100);
-   Assert.Equal(100, book.grades[0]);
-  }
- }
+        [Fact]
+        public void TestAddGrade()
+        {
+            var book = new InMemmoryBook("Grade Book");
+            book.AddGrade(100);
+            Assert.Equal(100, book.grades[0]);
+        }
+    }
 }
--- test/GradeBook.test/TypeTests.cs
+++ test/GradeBook.test/TypeTests.cs
@@ -4,147 +4,182 @@ using Xunit;
 namespace GradeBook.Test
 // ถ้าไม่ได้อยู่ใน GrandeBook จะต้อง using ด้วย
 {
+    public delegate string WriteLogDelegate(string logMessage);
+    public class TypeTests
+    {
+        #region Reference types
 
- public delegate string WriteLogDelegate(string logMessage);
- public class TypeTests
- {
+        // 1 - Can modify from reference!
+        [Fact]
+        public void CanSetNameFromReference()
+        {
+            var book1 = GetBook("Book 1");
+            SetName(book1, "New Name"); // Address in book1 is copied into book
 
-  int count = 0;
+            Assert.Equal("New Name", book1.Name);
 
-  [Fact]
-  public void WriteLogDelegateCanPoinToMethod()
-  {
-   WriteLogDelegate log = ReturnMessage;
+        }
 
-   //Point to any method that returns string and take a string.
-   // Must satisfy the delegate type!
-   //1st way - long hand notation
-   // log = new WriteLogDelegate(ReturnMessage);
+        private void SetName(Book book, string name)
+        {
+            book.Name = name;
+        }
 
-   //2nd way - shorter
-   log += ReturnMessage;
-   log += IncrementCount;
-   // invoked ReturnMessage x 2 and IncrementCount x1
-   var result = log("Hello!");
-   Assert.Equal(3, count);
-   Assert.Equal("hello!", result);
 
-  }
+        // 2 - Cannot set original ref to new ref! (inside the method)
+        [Fact]
+        public void CSharpIsPassByValue()
+        {
+            var book1 = GetBook("Book 1");
+            // When we invoke this... we make a copy of book1 --> to book
+            GetBookSetName(book1, "New Name");
 
-  string IncrementCount(string message)
-  {
-   count++;
-   return message.ToLower();
-  }
+            Assert.Equal("Book 1", book1.Name); // Remain the same!
+        }
 
-  string ReturnMessage(string message)
-  {
-   count++;
-   return message;
-  }
+        private void GetBookSetName(Book book, string name)
+        {
+            book = new InMemmoryBook(name); // try to set to new reference
+            book.Name = name; // and modify name
+            // No effect on original book object!
+        }
 
-  [Fact]
-  public void StringsBehaveLikeValueType()
-  {
-   string name = "Sirawit";
-   string upper = MakeUpperCase(name);
 
-   Assert.Equal("Sirawit", name);
-   Assert.Equal("SIRAWIT", upper);
+        // 3 - Can pass by reference if you want!
+        [Fact]
+        public void CSharpCanPassByRef()
+        {
+            var book1 = GetBook("Book 1");
+            GetBookSetName(ref book1, "New Name");
+            Assert.Equal("New Name", book1.Name);
+        }
 
-  }
+        private void GetBookSetName(ref Book book, string name)
+        {
+            book = new InMemmoryBook(name);
+        }
 
-  private string MakeUpperCase(string parameter)
-  {
-   // string are immutable ถึงมี ref ให้ แต่ก็แก้ไม่ได้เหมือน ref type อื่น ๆ
-   return parameter.ToUpper();
-  }
+        #endregion
 
-  [Fact]
-  public void ValueTypesAlsoPassByValue()
-  {
-   var x = GetInt();
-   SetInt(ref x);
-   Assert.Equal(42, x);
-  }
+        #region Value types
 
-  private void SetInt(ref int x)
-  {
-   x = 42;
-  }
+        [Fact]
+        public void ValueTypesAlsoPassByValue()
+        {
+            var x = 3;
+            SetInt(x);
+            Assert.Equal(3, x);
+        }
 
-  private int GetInt()
-  {
-   return 3;
-  }
+        private void SetInt(int z)
+        {
+            z = 42;
+        }
 
-  [Fact]
-  public void CSharpCanPassByRef()
-  {
-   var book1 = GetBook("Book 1");
-   GetBookSetName(ref book1, "New Name");
-   Assert.Equal("New Name", book1.Name);
-  }
+        // Value types (using ref)
+        [Fact]
+        public void ValueTypesAlsoPassByValueRef()
+        {
+            var x = 3;
+            SetInt(ref x);
+            Assert.Equal(42, x);
+        }
 
-  private void GetBookSetName(ref Book book, string name)
-  {
-   book = new InMemmoryBook(name);
-  }
+        private void SetInt(ref int z)
+        {
+            z = 42;
+        }
 
-  [Fact]
-  public void CSharpPassByValue()
-  {
-   var book1 = GetBook("Book 1");
-   // When we invoke this... we make a copy of book1 --> to book
-   GetBookSetName(book1, "New Name");
-   Assert.Equal("Book 1", book1.Name);
-  }
+        // Special case for String
+        // String is Reference type but behaves like a value type
+        // ถึงแม้จะได้ pointer ก็จริง แต่ก็ไม่มีทางที่จะเปลี่ยนค่าได้!
 
-  private void GetBookSetName(Book book, string name)
-  {
-   book = new InMemmoryBook(name);
-   book.Name = name;
-  }
+        [Fact]
+        public void StringsBehaveLikeValueTypes()
+        {
+            // Value types are really immutable!
+            // int i = 3;
+            // then i = 12; <--- donesn't change but instead replace!
 
-  [Fact]
-  public void CanSetFromReference()
-  {
-   var book1 = GetBook("Book 1");
-   SetName(book1, "New Name");
+            // String is immutable!
 
-   Assert.Equal("New Name", book1.Name);
+            string name = "Scott";
+            MakeUpperCase(name);
 
-  }
+            Assert.Equal("SCOTT", name); // this test failed!
 
-  private void SetName(Book book, string name)
-  {
-   book.Name = name;
-  }
+        }
 
-  [Fact]
-  public void GetBookReturnsDifferentObjects()
-  {
-   var book1 = GetBook("Book 1");
-   var book2 = GetBook("Book 2");
+        private void MakeUpperCase(string param)
+        {
+            param.ToUpper();
+        }
 
-   Assert.Equal("Book 1", book1.Name);
-   Assert.Equal("Book 2", book2.Name);
-   Assert.NotSame(book1, book2);
-  }
 
-  [Fact]
-  public void TwoVarsCanReferenceSameObject()
-  {
-   var book1 = GetBook("Book 1");
-   var book2 = book1;
+        [Fact]
+        public void GetBookReturnsDifferentObjects()
+        {
+            var book1 = GetBook("Book 1");
+            var book2 = GetBook("Book 2");
 
-   Assert.Same(book1, book2);
-   Assert.True(Object.ReferenceEquals(book1, book2));
-  }
+            Assert.Equal("Book 1", book1.Name);
+            Assert.Equal("Book 2", book2.Name);
+            Assert.NotSame(book1, book2);
+        }
 
-  Book GetBook(string name)
-  {
-   return new InMemmoryBook(name);
-  }
- }
+        [Fact]
+        public void TwoVarsCanReferenceSameObject()
+        {
+            var book1 = GetBook("Book 1");
+            var book2 = book1;
+
+            Assert.Same(book1, book2);
+            Assert.True(Object.ReferenceEquals(book1, book2));
+        }
+
+        Book GetBook(string name)
+        {
+            return new InMemmoryBook(name);
+        }
+
+        #endregion
+
+        #region Deligates
+
+        int count = 0;
+
+        [Fact]
+        public void WriteLogDelegateCanPoinToMethod()
+        {
+            WriteLogDelegate log = ReturnMessage;
+
+            //Point to any method that returns string and take a string.
+            // Must satisfy the delegate type!
+            //1st way - long hand notation
+            // log = new WriteLogDelegate(ReturnMessage);
+
+            //2nd way - shorter
+            log += ReturnMessage;
+            log += IncrementCount;
+            // invoked ReturnMessage x 2 and IncrementCount x1
+            var result = log("Hello!");
+            Assert.Equal(3, count);
+            Assert.Equal("hello!", result);
+
+        }
+
+        string IncrementCount(string message)
+        {
+            count++;
+            return message.ToLower();
+        }
+
+        string ReturnMessage(string message)
+        {
+            count++;
+            return message;
+        }
+
+        #endregion
+    }
 }
